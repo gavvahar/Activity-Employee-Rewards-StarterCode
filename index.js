c6bfdc29d737d6a5e84a7f2d06795fd6e03e86b9
gavvahar <nihargavva@gmail.com>
2022-06-14 14:23:29 -0400

    finished with the bonus

--- index.js
+++ index.js
@@ -27,44 +27,54 @@ for(let x = 0; x < banker.length; x++)
     let empId = banker[x][0]; //employee Id number
     let empChecking = banker[x][1]; //number of new checking
     let empYears = banker[x][2]; //years employed
-    /*if(empYears <= 1)
+    switch(true)
     {
-        if(empChecking >=1)
-        {
-            console.log("Ship $50.00 Gift Card");
-        }else
-        {
-            console.log("Incentives not met");
-        }
-    }else if(empYears == 2)
-    {
-        if(empChecking >= 3)
-        {
-            console.log("Ship $400.00 Gift Card");
-        }else
-        {
-            console.log("Ship $100.00 Gift Card");
-        }
-    }else if(empYears == 3)
-    {
-        if(empChecking >= 2)
-        {
-            console.log("Ship $700.00 Gift Card");
-        }else
-        {
-            console.log("Ship $350.00 Gift Card");
-        }
-    }else
-    {
-        if(empChecking >= 1)
-        {
-            console.log("Ship $3500.00 Gift Card");
-        }else
-        {
-            console.log("Ship set of knives");
-        }
-    }*/
-    
+        case (empYears <= 1):
+            switch(true)
+            {
+                default:
+                    console.log("Incentives not met");
+                    break;
+                case (empChecking >= 1):
+                    console.log("Ship $50.00 Gift Card");
+                    break;
+            }
+            break;
+        case (empYears == 2):
+            switch(true)
+            {
+                default:
+                    console.log("Ship $100.00 Gift Card");
+                    break;
+                case (empChecking >= 1):
+                    console.log("Ship $400.00 Gift Card");
+                    break;
+            }
+            break;
+        case (empYears == 3):
+            switch(true)
+            {
+                default:
+                    console.log("Ship $350.00 Gift Card");
+                    break;
+                case (empChecking >= 1):
+                    console.log("Ship $700.00 Gift Card");
+                    break;
+            }
+            break;
+        case (empYears >= 4):
+            switch(true)
+            {
+                default:
+                    console.log("Ship set of knives");
+                    break;
+                case (empChecking >= 1):
+                    console.log("Ship $3500.00 Gift Card");
+                    break;
+            }
+            break;
+    }
+
     // Output when activity is complete
     console.log(`${empId}: Processed`)
 }
