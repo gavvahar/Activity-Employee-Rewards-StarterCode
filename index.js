88cac7bba2030ce662d37c90f034d046b333eab6
gavvahar <nihargavva@gmail.com>
2022-06-14 13:31:21 -0400

    finished with this

--- index.js
+++ index.js
@@ -17,44 +17,53 @@ Otherwise output:'Ship set of knives'
 
 
 // Variable declaration
-let associateBanker = [56123, 5, 1]; //employeeId, checking, years
-let empId = associateBanker[0]       //employee Id number
-let empChecking = associateBanker[1] //number of new checking 
-let empYears = associateBanker[2];   //years employed
-
-// Continue same pattern/naming convention for each banker
-// leaving only one variable declaration section uncommented each 'Run'
-
-/* 
-let seniorBanker = [77227, 1, 2]
-let empId = seniorBanker[0]       
-let empChecking = seniorBanker[1]
-let empYears = seniorBanker[2];   
-*/
-
+const banker = [
+    [56123, 5, 1], [77227, 1, 2], [66225, 5, 3], [56677, 5, 0], [22151, 2, 6]
+]; //employeeId, checking, years
 
 //<--Your code goes here-->
-if(empYears == 1)
+for(let x = 0; x < banker.length; x++)
 {
-    if(empChecking >= 1)
+    let empId = banker[x][0]; //employee Id number
+    let empChecking = banker[x][1]; //number of new checking
+    let empYears = banker[x][2]; //years employed
+    if(empYears <= 1)
     {
-        console.log("Ship $50.00 Gift Card")
+        if(empChecking >=1)
+        {
+            console.log("Ship $50.00 Gift Card");
+        }else
+        {
+            console.log("Incentives not met");
+        }
+    }else if(empYears == 2)
+    {
+        if(empChecking >= 3)
+        {
+            console.log("Ship $400.00 Gift Card");
+        }else
+        {
+            console.log("Ship $100.00 Gift Card");
+        }
+    }else if(empYears == 3)
+    {
+        if(empChecking >= 2)
+        {
+            console.log("Ship $700.00 Gift Card");
+        }else
+        {
+            console.log("Ship $350.00 Gift Card");
+        }
     }else
     {
-        console.log("Incentives not met")
+        if(empChecking >= 1)
+        {
+            console.log("Ship $3500.00 Gift Card");
+        }else
+        {
+            console.log("Ship set of knives");
+        }
     }
-}else if(empYears == 2)
-{
-    if(empChecking >=3)
-    {
-        console.log("Ship $50.00 Gift Card")
-    }else
-    {
-        console.log("Ship $100.00 Gift Card")
-    }
+    // Output when activity is complete
+    console.log(`${empId}: Processed`)
 }
-// Output when activity is complete
-console.log(`${empId}: Processed`)
-
-    
-   
